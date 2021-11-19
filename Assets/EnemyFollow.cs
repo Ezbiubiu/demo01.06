e578db2c8e5203302f1fef39d7ac9df5fb6ec7a8
Bebop <ezbiubiu@gmail.com>
2021-11-18 20:50:16 -0600

    1.06.02

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -8,13 +8,15 @@ public class EnemyFollow : MonoBehaviour
     public float speed;
 
     private Transform playerPos;
+    private Vector2 moveTo;
 
-
+    private Rigidbody2D rb;
 
 
     void Awake()
     {
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform;      
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;  
+        rb = GetComponent<Rigidbody2D>();    
         
     }
 
@@ -24,9 +26,22 @@ public class EnemyFollow : MonoBehaviour
         if (Vector2.Distance(transform.position, playerPos.position) < 10f) // enemy will stay if distance longer than 10f
         {
             if(Vector2.Distance(transform.position,playerPos.position)>0.3f) // enemy will not be inside your body
-                transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime); 
+                // transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime); 
+                velocity = playerPos.position - transform.position;
         }
 
+    }
+
+    private void Flip() // enemy pic will flip follow mouse's position
+    {
+        //mouse is on the right side of the zombie
+        if(transform.position.x < Camera.main.ScreenToWorldPoint(Input.mousePosition).x)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
+        //Mouse is on the left side of the zombie
+        if(transform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+               
     }
 
 }
