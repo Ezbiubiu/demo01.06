4ebecd634b2f12f21e3009ae80acce732637a1c6
slongxin <longxinshi@gmail.com>
2021-11-28 16:24:00 -0600

    test boss, dont merge

---
+++ Assets/BossBehavior.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// public enum BossState 
+// {
+//     FireBall,
+//     FirePillar,
+//     Dash,
+//     Idle,
+//     BeHit,
+//     Death,
+// }
+
+// BossState state;
+
+
+// public class BossBehavior : MonoBehaviour
+// {
+
+//     public GameObject Fireball;
+
+//     public int FireBallAttackTime;
+
+//     public bool isDead;
+
+
+//     void Awake()
+//     {
+//         state = BossState.Idle;
+//         isDead = false;
+//         FireBallAttackTime = 3;
+//     }
+
+//     void Update()
+//     {
+//         switch(state)
+//         {
+//             case BossState.FireBall:
+//             {
+//                 FireBallAttack();
+//                 break;
+//             }
+
+//         }
+//     }
+
+//     public void FireBallAtttack()
+//     {
+        
+
+//     }
+    
+}
