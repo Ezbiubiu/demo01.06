df0251328eb3abdb4f6f3a75206490e0704c8b2d
Bebop <ezbiubiu@gmail.com>
2021-12-01 23:36:45 -0600

    111

--- Assets/DropGift.cs
+++ Assets/DropGift.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drop-Gift : MonoBehaviour
+public class DropGift : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,12 @@ public class Drop-Gift : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void getGift()
+    {
+        Vector3 pos = transform.position;
+        Instantiate(gos[Random.Range(0, gos.Length)], pos, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
